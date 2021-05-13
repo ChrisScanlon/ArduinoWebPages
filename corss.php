4cefba4578913a46b669a32aab932b73596ffb19
Chris Scanlon <9209301+ChrisScanlon@users.noreply.github.com>
2021-05-13 23:02:14 +1000

    Update corss.php

--- corss.php
+++ corss.php
@@ -2,6 +2,6 @@
 
 header('Content-type: application/jsonp');
 header('Access-Control-Allow-Origin: *');
-echo file_get_contents("https://github.com/ChrisScanlon/ArduinoWebPages/master/JSON2.JSON");
+echo file_get_contents("https://github.com/ChrisScanlon/ArduinoWebPages/JSON2.JSON");
 
 ?>
