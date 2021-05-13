4d392ffbf7326d64c3b79ad22a8e9ae24b7a5ef7
Chris Scanlon <9209301+ChrisScanlon@users.noreply.github.com>
2021-05-13 15:16:53 +1000

    Create ScanFolders.php
    This php is designed to scan the directory and list details to determine if the files have been updated and require download to the webserver

---
+++ ScanFolders.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Represent a custom directory class call DirectoryLister where path is detailed
+ */
+class DirectoryLister implements JsonSerializable{
+    
+    private $path;
+
+    /**
+     * Instanciate a Directory with a path
+     * @param string $path
+     */
+    function __construct(string $path){
+        //check before if there are any files that exist
+        if(file_exists($path)){
+            $this->path = $path;
+        }else{
+            throw \Exception("Files do not exist for this path: $path");
+        }
+    }
+    
+    /**
+     * Return filenames contained within the directory
+     */
+    function walk(){
+        // scan directory and  
+        foreach( scandir($this->path) as $basename){
+            $new_path = $this->path.DIRECTORY_SEPARATOR.$basename ;
+            // TODO: add a regex test
+            if(!in_array($basename, ['.', '..']) && file_exists($new_path) ){
+                yield $new_path;
+            }
+        }
+    }
+    
+    /**
+     * Convert this object into an array
+     * @return array
+     */
+    function to_a(&$directories=null):array{
+        if(!$directories){
+            $directories = [];
+        }
+        
+        foreach($this->walk() as $file){
+            
+            // if it's a directory, we loop on it
+            if(is_dir($file)){
+                $sub_dir = new DirectoryLister($file);
+                $name = basename($file) ;
+                $directories[$name] = [$name =>  $sub_dir->to_a($directories[$name]) ] ;
+            // if it's a file
+            }else{
+                $name = basename($file) ;
+                array_push($directories, $name);
+            }  
+        }
+        return $directories;
+        
+    }
+    
+    /**
+     * Overwritted function to specify json_encode() output
+     * @return array
+     */
+    function jsonSerialize(){
+        $array = $this->to_a();
+        return $array;
+    }
+    
+    function get_basename():string{return basename($this->path);}
+
+    
+}
+
+$dir = new DirectoryLister('https://github.com/ChrisScanlon/ArduinoWebPages/');
+$json =  json_encode($dir, JSON_PRETTY_PRINT);
+echo $json ;
